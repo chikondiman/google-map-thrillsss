01d82023f2620ae980584604244a4ad959cb7163
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-10-12 13:12:51 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,5 +25,5 @@ export class MapContainer extends Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'YOUR_GOOGLE_MAPS_API_KEY_GOES_HERE'
+  apiKey: 'AIzaSyAujYLcIe3uJwCU54Kt6Hb61SZnb0apMmU'
 })(MapContainer);
