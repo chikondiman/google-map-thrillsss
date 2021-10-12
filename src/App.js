983b40c9b13a9212e717845e5f98b9d1b40b3b1f
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-10-12 13:17:18 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export class MapContainer extends Component {
     activeMarker: {},          // Shows the active marker upon click
     selectedPlace: {}          // Shows the InfoWindow to the selected place upon a marker
   };
-  
+
   onMarkerClick = (props, marker, e) =>
     this.setState({
       selectedPlace: props,
@@ -32,16 +32,31 @@ export class MapContainer extends Component {
   render() {
     return (
       <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={
-          {
-            lat: -1.2884,
-            lng: 36.8233
-          }
+      google={this.props.google}
+      zoom={14}
+      style={mapStyles}
+      initialCenter={
+        {
+          lat: -1.2884,
+          lng: 36.8233
         }
+      }
+    >
+      <Marker
+        onClick={this.onMarkerClick}
+        name={'Kenyatta International Convention Centre'}
       />
+      <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}
+        onClose={this.onClose}
+      >
+        <div>
+          <h4>{this.state.selectedPlace.name}</h4>
+        </div>
+      </InfoWindow>
+    </Map>
+
     );
   }
 }
